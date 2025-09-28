41bd6aeaeb3743c1302f3fd1e0bdd85ca73ee466
Haroxgaming <mathis.denamur.pro@gmail.com>
2025-09-28 17:15:24 +0200

    blocking level 1 & create ennemy
    blocking level 1 & create ennemy

---
+++ Source/Project_RType/Private/EnnemyParent.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnnemyParent.h"
+
+// Sets default values
+AEnnemyParent::AEnnemyParent()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AEnnemyParent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AEnnemyParent::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
